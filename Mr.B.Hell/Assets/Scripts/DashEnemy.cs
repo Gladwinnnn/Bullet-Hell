118a9d3fdd08b875b32f78c90e275a6e0a09aad5
Gladwin <gladwin1503@hotmail.com>
2022-05-24 12:24:12 +0800

    Basic movement of dash enemy
    Completed the basic movement of dash enemy as well as hid the enemy spawner for easier testing

--- Mr.B.Hell/Assets/Scripts/DashEnemy.cs
+++ Mr.B.Hell/Assets/Scripts/DashEnemy.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MeleeEnemy : MonoBehaviour
+public class DashEnemy : MonoBehaviour
 {
     [Header("Enemy Stats")]
     [SerializeField] float moveSpeed = 1f;
     [SerializeField] int lifePoints = 5;
-    [SerializeField] int meleeDamage = 1;
+    [SerializeField] int dashDamage = 1;
 
     [SerializeField] GameObject player;
+
+    float countDownToDash = 1f, dashDuration = 0.5f, speed = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +23,7 @@ public class MeleeEnemy : MonoBehaviour
     void Update()
     {
         Move();
+        SuddenDash();
 
         if (lifePoints == 0)
         {
@@ -33,6 +36,29 @@ public class MeleeEnemy : MonoBehaviour
         var targetPosition = player.transform.position;
         var movementThisFrame = moveSpeed * Time.deltaTime;
         transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+    }
+
+    void SuddenDash()
+    {
+        if (countDownToDash <= 0)
+        {
+            moveSpeed = 5f;
+
+            if (dashDuration <= 0)
+            {
+                moveSpeed = 1f;
+                countDownToDash = 2f;
+                dashDuration = 0.5f;
+            }
+            else
+            {
+                dashDuration -= Time.deltaTime * speed;
+            }
+        }
+        else
+        {
+            countDownToDash -= Time.deltaTime * speed;
+        }
     }
 
     void Die() 
