6276b91b6ee8a62dbf00f92932332337d11fbf29
Gladwin <gladwin1503@hotmail.com>
2022-05-24 14:43:46 +0800

    finished basic movement of shooter enemy

---
+++ Mr.B.Hell/Assets/Scripts/ShooterEnemy.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterEnemy : MonoBehaviour
+{
+    [Header("Enemy Stats")]
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] int lifePoints = 5;
+    [SerializeField] int shootDamage = 1;
+
+    [Header("Projectile")]
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    float countDownToFire = 1.5f, fireDuration = 0.01f, speed = 1f;
+
+    [SerializeField] GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Fire();
+
+        if (lifePoints == 0)
+        {
+            Die();
+        }
+    }
+
+    void Move()
+    {
+        var targetPosition = player.transform.position;
+        var movementThisFrame = moveSpeed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+    }
+
+    void Fire()
+    {
+        if (countDownToFire <= 0)
+        {
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+            laser.GetComponent<Rigidbody2D>().velocity = (player.transform.position - transform.position).normalized * projectileSpeed;
+
+            if (fireDuration <= 0)
+            {
+                countDownToFire = 1.5f;
+                fireDuration = 0.01f;
+            }
+            else
+            {
+                fireDuration -= Time.deltaTime * speed;
+            }
+        }
+        else
+        {
+            countDownToFire -= Time.deltaTime * speed;
+        }
+    }
+
+    void Die() 
+    {
+        Destroy(gameObject);
+    }
+}
