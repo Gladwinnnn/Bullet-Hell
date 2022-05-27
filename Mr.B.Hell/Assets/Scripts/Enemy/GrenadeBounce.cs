3c418c3633f71bb6f89bdbc9d54a962686613229
Gladwin <gladwin1503@hotmail.com>
2022-05-27 21:03:29 +0800

    Update GrenadeBounce.cs

--- Mr.B.Hell/Assets/Scripts/Enemy/GrenadeBounce.cs
+++ Mr.B.Hell/Assets/Scripts/Enemy/GrenadeBounce.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class GrenadeBounce : MonoBehaviour
 {
+    Vector3 lastVelocity;
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        lastVelocity = rb.velocity;
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var speed = lastVelocity.magnitude;
+        var dir = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+        rb.velocity = dir * speed;
     }
 }
