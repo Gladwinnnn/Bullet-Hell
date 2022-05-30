b46ed9fbefa1efd739995ea40a7c6fcab82ec494
Gladwin <gladwin1503@hotmail.com>
2022-05-30 12:15:11 +0800

    Update EnemySpawner.cs

--- Mr.B.Hell/Assets/Scripts/Enemy/EnemySpawner.cs
+++ Mr.B.Hell/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -12,13 +12,24 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] Vector2 playArea;
 
     int numberOfEnemies;
+    float countDown = 60f;
+    bool spawnNextWave = false;
 
     IEnumerator Start()
     {
+        countDown -= Time.deltaTime;
+
         while(spawn)
         {
             yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
-            if (numberOfEnemies <= 20)
+            if (countDown <= 0) spawnNextWave = true;
+            
+            if (numberOfEnemies <= 20 && !spawnNextWave)
+            {
+                SpawnFirstWave();
+                numberOfEnemies++;
+            }
+            else if (numberOfEnemies <= 20 && spawnNextWave)
             {
                 SpawnEnemies();
                 numberOfEnemies++;
@@ -38,5 +49,13 @@ public class EnemySpawner : MonoBehaviour
         Vector2 spawnLocation = new Vector2(randomXCoordinates, randomYCoordinates);
         int enemyToSpawn = Random.Range(0,enemy.Length);
         Instantiate(enemy[enemyToSpawn], spawnLocation, Quaternion.identity);
+    }
+
+    void SpawnFirstWave()
+    {
+        float randomYCoordinates = Random.Range(-playArea.x, playArea.x);
+        float randomXCoordinates = Random.Range(-playArea.y, playArea.y);
+        Vector2 spawnLocation = new Vector2(randomXCoordinates, randomYCoordinates);
+        Instantiate(enemy[0], spawnLocation, Quaternion.identity);
     }
 }
