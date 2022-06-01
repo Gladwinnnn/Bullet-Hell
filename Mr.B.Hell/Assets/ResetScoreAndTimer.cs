e4317e36d0bda74d02f4f55d15e7534eee161b30
sohwathismelvin <melvinsoh@yahoo.com.sg>
2022-06-01 15:17:01 +0800

    timer and score reset fix

--- Mr.B.Hell/Assets/ResetScoreAndTimer.cs
+++ Mr.B.Hell/Assets/ResetScoreAndTimer.cs
@@ -7,14 +7,21 @@ public class ResetScoreAndTimer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("RESET");
-        FindObjectOfType<Timer>().ResetTimer();
-        FindObjectOfType<GameSession>().ResetScore();
+        Invoke("ResetScores", 0.1f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    void ResetScores()
+    {
+        //print(FindObjectsOfType<Timer>().Length);
+
+        FindObjectOfType<Timer>().ResetTimer();
+        FindObjectOfType<GameSession>().ResetScore();
     }
 }
--- Mr.B.Hell/Assets/Scripts/Timer.cs
+++ Mr.B.Hell/Assets/Scripts/Timer.cs
@@ -43,6 +43,7 @@ public class Timer : MonoBehaviour
     }
     public void Finish()
     {
+        print("calling finish");
         playing = false;
         finishTime = timerText.text;
     }
